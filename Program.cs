04d44521a9bbc5930bdf71a2bc099de39c18036d
JohnEspinal <52285339+JohnEspinal@users.noreply.github.com>
2020-01-30 19:47:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionForPractice
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] input = new int[5];
+            string[] inputString = new string[5];
+            int[] inputFirst = new int[5];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = Convert.ToInt32(Console.ReadLine());
+
+                inputString[i] = input[i].ToString();
+            }
+
+
+            
+
+
+            foreach (string element in inputString)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    inputFirst[i] = Convert.ToInt32(element[i].ToString());
+                }
+
+
+            }
+        }
+
+
+    
+    }
+}
