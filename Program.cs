d0db6af5a7330b54bd2f39a503d1ae2187df37c2
JohnEspinal <52285339+JohnEspinal@users.noreply.github.com>
2020-02-06 00:28:12 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,44 +1,87 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SolutionForPractice
+namespace ConsoleApp
 {
     class Program
     {
-        
-
         static void Main(string[] args)
         {
-            int[] input = new int[5];
-            string[] inputString = new string[5];
-            int[] inputFirst = new int[5];
+            //Tomar 5 inputs y devolver el mayor numero posible ordenando cada NUMERO sin alterar el orden de los digitos
 
-            for (int i = 0; i < input.Length; i++)
+            string[] input = new string[5];
+            string temp = "";
+            string element = "";
+            string elementNext = "";
+            string result = "";
+
+            for (int i = 0; i < 5; i++)
             {
-                input[i] = Convert.ToInt32(Console.ReadLine());
-
-                inputString[i] = input[i].ToString();
+                input[i] = Console.ReadLine();
             }
-
 
             
 
-
-            foreach (string element in inputString)
+            for (int i = 0; i < 4; i++)
             {
-                for (int i = 0; i < input.Length; i++)
+
+
+                element = input[i];
+                elementNext = input[i + 1];
+
+                if(element.Length >= elementNext.Length)
                 {
-                    inputFirst[i] = Convert.ToInt32(element[i].ToString());
+                    for (int x = 0; x < elementNext.Length; x++)
+                    {
+                        //Comparar si el primer digito de un numero es mayor que el primer digito del otro
+
+                        if (element[x] < elementNext[x])
+                        {
+                            temp = element;
+                            input[i] = elementNext;
+                            input[i + 1] = temp;
+                            break;
+                        }
+                        else if(element[x] > elementNext[x])
+                        {
+                            break;
+                        }
+                        
+                    }
+                }
+
+                if (element.Length < elementNext.Length)
+                {
+                    for (int x = 0; x < element.Length; x++)
+                    {
+                        if (element[x] < elementNext[x])
+                        {
+                            temp = element;
+                            input[i] = elementNext;
+                            input[i + 1] = temp;
+                        }
+                        else if (element[x] > elementNext[x])
+                        {
+                            break;
+                        }
+                    }
                 }
 
 
             }
+
+            foreach (string numero in input)
+            {
+                result += numero;
+            }
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
         }
-
-
-    
     }
 }
